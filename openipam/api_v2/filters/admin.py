5254388518a90c04069498ce5dab3c7b24324fd0
Treyson <treyson.grange@outlook.com>
2024-06-05 14:13:13 -0600

    log api now filters

--- openipam/api_v2/filters/admin.py
+++ openipam/api_v2/filters/admin.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework as filters
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
 from openipam.log.models import EmailLog, DnsRecordsLog, HostLog, AddressLog, UserLog
+from django.contrib.auth.models import Group
 
 
 class LogEntryFilterSet(filters.FilterSet):
@@ -27,3 +28,51 @@ class EmailLogFilterSet(filters.FilterSet):
     class Meta:
         model = EmailLog
         fields = ["to"]
+
+
+class HostLogFilterSet(filters.FilterSet):
+    hostname = filters.CharFilter(field_name="hostname")
+    mac = filters.CharFilter(field_name="mac")
+
+    class Meta:
+        model = HostLog
+        fields = ["hostname", "mac"]
+
+
+class AddressLogFilterSet(filters.FilterSet):
+    address = filters.CharFilter(field_name="address")
+    mac = filters.CharFilter(field_name="mac")
+
+    class Meta:
+        model = AddressLog
+        fields = ["address", "mac"]
+
+
+class UserLogFilterSet(filters.FilterSet):
+    username = filters.CharFilter(field_name="username")
+    first_name = filters.CharFilter(field_name="first_name")
+    last_name = filters.CharFilter(field_name="last_name")
+    email = filters.CharFilter(field_name="email")
+    is_staff = filters.BooleanFilter(field_name="is_staff")
+    is_superuser = filters.BooleanFilter(field_name="is_superuser")
+
+    class Meta:
+        model = UserLog
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_staff",
+            "is_superuser",
+        ]
+
+
+class DnsRecordsLogFilterSet(filters.FilterSet):
+    name = filters.CharFilter(field_name="name")
+    dns_type = filters.CharFilter(field_name="dns_type")
+    ip_content = filters.CharFilter(field_name="ip_content")
+
+    class Meta:
+        model = DnsRecordsLog
+        fields = ["name", "dns_type", "ip_content"]
--- openipam/api_v2/views/logs.py
+++ openipam/api_v2/views/logs.py
@@ -5,8 +5,14 @@ from django.contrib.admin.models import LogEntry
 from django_filters.rest_framework import DjangoFilterBackend
 from .base import APIModelViewSet, APIPagination
 from django_filters.rest_framework import DjangoFilterBackend
-from ..filters.admin import LogEntryFilterSet, EmailLogFilterSet
-from ..filters.base import FieldChoiceFilter
+from ..filters.admin import (
+    LogEntryFilterSet,
+    EmailLogFilterSet,
+    HostLogFilterSet,
+    AddressLogFilterSet,
+    UserLogFilterSet,
+    DnsRecordsLogFilterSet,
+)
 from ..permissions import APIAdminPermission
 from openipam.log.models import EmailLog, DnsRecordsLog, HostLog, AddressLog, UserLog
 from django.contrib.admin.models import LogEntry
@@ -29,13 +35,6 @@ class LogViewSet(APIModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = LogEntryFilterSet
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        to = self.request.query_params.get("to", None)
-        if to:
-            queryset = queryset.filter(to=to)
-        return queryset
-
     def get_serializer_class(self):
         return self.serializer_class
 
@@ -44,13 +43,21 @@ class LogViewSet(APIModelViewSet):
             return [permissions.IsAuthenticated()]
         return [permissions.IsAdminUser()]
 
-    @action(detail=False, methods=["get"])
+    @action(
+        detail=False,
+        methods=["get"],
+        queryset=HostLog.objects.all(),
+        filter_backends=[DjangoFilterBackend],
+        serializer_class=HostLogsSerializer,
+        filterset_class=HostLogFilterSet,
+    )
     def host(self, request: Request):
-        """List all host logs."""
+        """List all host logs with filtering."""
         queryset = HostLog.objects.all()
+        queryset = self.filter_queryset(queryset)
         pagination = APIPagination()
         page = pagination.paginate_queryset(queryset, request)
-        host_serializer = HostLogsSerializer(page, many=True)
+        host_serializer = self.get_serializer(page, many=True)
         return pagination.get_paginated_response(host_serializer.data)
 
     @action(
@@ -63,35 +70,56 @@ class LogViewSet(APIModelViewSet):
     )
     def email(self, request: Request):
         """List all email logs."""
-        email_logs = self.filter_queryset(self.get_queryset())
+        queryset = self.filter_queryset(EmailLog.objects.all())
         pagination = APIPagination()
-        page = pagination.paginate_queryset(email_logs, request)
+        page = pagination.paginate_queryset(queryset, request)
+        email_serializer = self.get_serializer(page, many=True)
+        return pagination.get_paginated_response(email_serializer.data)
+
+    @action(
+        detail=False,
+        methods=["get"],
+        queryset=DnsRecordsLog.objects.all(),
+        filter_backends=[DjangoFilterBackend],
+        serializer_class=DNSLogSerializer,
+        filterset_class=DnsRecordsLogFilterSet,
+    )
+    def dns(self, request: Request):
+        """List all DNS logs."""
+        queryset = self.filter_queryset(DnsRecordsLog.objects.all())
+        pagination = APIPagination()
+        page = pagination.paginate_queryset(queryset, request)
         dns_serializer = self.get_serializer(page, many=True)
         return pagination.get_paginated_response(dns_serializer.data)
 
-    @action(detail=False, methods=["get"])
-    def dns(self, request: Request):
-        """List all DNS logs."""
-        queryset = DnsRecordsLog.objects.all()
-        pagination = APIPagination()
-        page = pagination.paginate_queryset(queryset, request)
-        dns_serializer = DNSLogSerializer(page, many=True)
-        return pagination.get_paginated_response(dns_serializer.data)
-
-    @action(detail=False, methods=["get"])
+    @action(
+        detail=False,
+        methods=["get"],
+        queryset=AddressLog.objects.all(),
+        filter_backends=[DjangoFilterBackend],
+        serializer_class=AddressLogsSerializer,
+        filterset_class=AddressLogFilterSet,
+    )
     def address(self, request: Request):
         """List all address logs."""
-        queryset = AddressLog.objects.all()
+        queryset = self.filter_queryset(AddressLog.objects.all())
         pagination = APIPagination()
         page = pagination.paginate_queryset(queryset, request)
-        address_serializer = AddressLogsSerializer(page, many=True)
+        address_serializer = self.get_serializer(page, many=True)
         return pagination.get_paginated_response(address_serializer.data)
 
-    @action(detail=False, methods=["get"])
+    @action(
+        detail=False,
+        methods=["get"],
+        queryset=UserLog.objects.all(),
+        filter_backends=[DjangoFilterBackend],
+        serializer_class=UserLogsSerializer,
+        filterset_class=UserLogFilterSet,
+    )
     def user(self, request: Request):
         """List all user logs."""
-        queryset = UserLog.objects.all()
+        queryset = self.filter_queryset(UserLog.objects.all())
         pagination = APIPagination()
         page = pagination.paginate_queryset(queryset, request)
-        user_serializer = UserLogsSerializer(page, many=True)
+        user_serializer = self.get_serializer(page, many=True)
         return pagination.get_paginated_response(user_serializer.data)
