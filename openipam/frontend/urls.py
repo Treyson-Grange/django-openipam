a75815b83bb4fbba64ea1247f19356829321ce42
Treyson <treyson.grange@outlook.com>
2024-09-13 10:26:23 -0600

    rid 'frontend'

--- openipam/frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index
-from django.urls import re_path
-
-urlpatterns = [
-    path("", index, name="frontend"),
-    re_path(r"^.*$", index, name="frontend"),
-]
